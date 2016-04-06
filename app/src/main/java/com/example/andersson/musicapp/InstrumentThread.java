d32275fb40ceb1da2d0fb6a63c76f0d15f44c067
DAnd91 <davidandersson91@me.com>
2016-04-06 18:51:49 +0200

    Minor updates

--- app/src/main/java/com/example/andersson/musicapp/InstrumentThread.java
+++ app/src/main/java/com/example/andersson/musicapp/InstrumentThread.java
@@ -18,20 +18,14 @@ public abstract class InstrumentThread extends Thread {
     private int bars = 10;
     int i;
     private Calendar calendar;
+    public ArrayList<Integer> freqOfTone;
 
-    // Copy
-    private final double duration = 1; // seconds
-    private final int sampleRate = 8000;
-    private final int numSamples = (int) duration * sampleRate;
-    private final double sample[] = new double[numSamples];
-    private ArrayList<Integer> freqOfTone; // hz
-    private final double freqOfTone2 = 440;
-    private final byte generatedSnd[] = new byte[2 * numSamples];
-    //
+
+    public abstract void instrument(int index);
 
     public InstrumentThread(){
 
-        freqOfTone = new ArrayList<Integer>(Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0));
+        freqOfTone = new ArrayList<Integer>(Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
         i = 1;
 
     }
@@ -96,38 +90,6 @@ public abstract class InstrumentThread extends Thread {
     public void setSoundList(ArrayList<Integer> freqOfTone){
 
         this.freqOfTone = freqOfTone;
-    }
-
-    public abstract void instrument(int index);
-
-
-    void genTone(int index){
-        // fill out the array
-        for (int i = 0; i < numSamples; ++i) {
-            sample[i] = Math.sin(2 * Math.PI * i / (sampleRate/freqOfTone.get(index)));
-            //sample[i] = Math.sin(2 * Math.PI * i / (sampleRate/freqOfTone2));
-        }
-
-        // convert to 16 bit pcm sound array
-        // assumes the sample buffer is normalised.
-        int idx = 0;
-        for (final double dVal : sample) {
-            // scale to maximum amplitude
-            final short val = (short) ((dVal * 32767));
-            // in 16 bit wav PCM, first byte is the low order byte
-            generatedSnd[idx++] = (byte) (val & 0x00ff);
-            generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
-
-        }
-    }
-
-    void playSound(){
-        final AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
-                sampleRate, AudioFormat.CHANNEL_OUT_MONO,
-                AudioFormat.ENCODING_PCM_16BIT, generatedSnd.length,
-                AudioTrack.MODE_STATIC);
-        audioTrack.write(generatedSnd, 0, generatedSnd.length);
-        audioTrack.play();
     }
 
 }
